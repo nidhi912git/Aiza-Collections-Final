2d52e28524da8344d3407ff8a411618316fb6cfe
anithapatel1203 <120438003+anithapatel1203@users.noreply.github.com>
2026-03-10 16:19:51 +0530

    css changed

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,7 @@ include_once __DIR__ . "/security.php";
 <title>Aiza Collections</title>
 
 <link rel="stylesheet" href="/aiza-collections-final/css/style.css?v=<?= time() ?>">
-<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&family=Poppins:wght@300;400;400;500;600&display=swap" rel="stylesheet">
 <script src="/aiza-collections-final/js/script.js?v=<?= time() ?>" defer></script>
 </head>
 <body id="<?= $page_id ?? '' ?>" class="<?= $page_id ?? '' ?> <?= strpos($page_id,'admin')===0?'admin-page':'' ?>">
